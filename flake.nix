64e6b5b507ceded9e92f66152bc60b51582a6470
Jordan Mackie <jmackie@protonmail.com>
2023-02-13 18:37:37 +0000

    wasm-pack new
    With some pruning...

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, fenix, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          fenixPackages = fenix.packages.${system};
+          rustToolchain = fenixPackages.fromToolchainFile {
+            file = ./rust-toolchain.toml;
+            sha256 = "sha256-PZbOSw+520ocE9LBzkwzEduQTqqNstPb4VdCzl3FNwk=";
+          };
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.wasm-pack
+              pkgs.wasm-bindgen-cli
+              pkgs.binaryen
+              rustToolchain
+            ];
+          };
+
+        }
+      );
+}
